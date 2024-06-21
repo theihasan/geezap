7fa49e1842da5f780279351335553b3c08f90919
Md Abul Hassan <imabulhasan99@gmail.com>
2024-06-21 06:29:47 +0600

    upgrade all job class to a config file base

---
+++ config/geezap.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    'laravel' => [
+        'query' => 'Laravel',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/laravel.png'),
+    ],
+
+    'symfony' => [
+        'query' => 'Symfony',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/symfony.png'),
+    ],
+
+    'wordpress' => [
+        'query' => 'Wordpress',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/wordpress.png'),
+    ],
+    'vuejs' => [
+        'query' => 'Vue Js',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/vuejs.png'),
+    ],
+    'reactjs' => [
+        'query' => 'React JS',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/reactjs.png'),
+    ],
+    'nodejs' => [
+        'query' => 'Node JS',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/nodejs.png'),
+    ],
+    'python' => [
+        'query' => 'Python',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/python.png'),
+    ],
+    'asp' => [
+        'query' => 'Asp.net',
+        'num_pages' => 20,
+        'date_posted' => 'week',
+        'page'  => 2,
+        'category_image' => asset('assets/images/categories/asp.png'),
+    ],
+];
