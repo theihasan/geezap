ae7fdf698e5d840126106d6a22384d68302ae91a
Md Abul Hassan <imabulhasan99@gmail.com>
2024-09-03 00:56:11 +0600

    add more job category

--- app/Enums/JobCategory.php
+++ app/Enums/JobCategory.php
@@ -16,9 +16,33 @@ enum JobCategory :string
     case Spring = 'spring';
     case RubyOnRails = 'ruby-on-rails';
     case ASPNET = 'aspnet';
-    case PHP = 'php';
     case NodeJS = 'nodejs';
     case Python = 'python';
+    case PHP = 'php';
+    case Java = 'java';
+    case CSharp = 'csharp';
+    case Ruby = 'ruby';
+    case Go = 'go';
+    case Swift = 'swift';
+    case Kotlin = 'kotlin';
+    case Rust = 'rust';
+    case Scala = 'scala';
+    case TypeScript = 'typescript';
+    case JavaScript = 'javascript';
+    case HTML = 'html';
+    case CSS = 'css';
+    case SQL = 'sql';
+    case NoSQL = 'nosql';
+    case MongoDB = 'mongodb';
+    case MySQL = 'mysql';
+    case PostgreSQL = 'postgresql';
+    case SQLite = 'sqlite';
+    case Oracle = 'oracle';
+    case MariaDB = 'mariadb';
+    case Redis = 'redis';
+    case Elasticsearch = 'elasticsearch';
+    case Kafka = 'kafka';
+    case RabbitMQ = 'rabbitmq';
 
 
 }
