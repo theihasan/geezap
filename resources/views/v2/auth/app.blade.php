2e3358c9fcfb67ede33bea4a2f154108659ff3a0
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-09 02:41:46 +0600

    implemented new register page layout

---
+++ resources/views/v2/auth/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - DevJobs</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+</head>
+<body class="bg-[#0A0A1B] flex items-center justify-center min-h-screen">
+
+@yield('content')
+
+
+</body>
+</html>
