0fc468522172be946166a909804491e13dd74a14
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-11 01:21:45 +0600

    add status column for job user table

--- database/migrations/2024_11_10_013327_create_job_student_table.php
+++ database/migrations/2024_11_10_013327_create_job_student_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('job_id')->constrained('job_listings')->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('status');
             $table->timestamps();
         });
     }
