03cd77d662a935c091b56f2a9e140479d1c0fee6
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-13 02:19:53 +0600

    setup filament service provider with some minor setup

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,6 +50,7 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+            ->topNavigation()
             ->authMiddleware([
                 Authenticate::class,
             ]);
