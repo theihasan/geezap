f78cfae828990bc832636cacdfd9443c69b0b918
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-13 01:19:14 +0600

    initial filament setup for admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
 use App\Observers\UserObserver;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 #[ObservedBy([UserObserver::class])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -20,33 +24,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'address',
-        'dob',
-        'state',
-        'country',
-        'occupation',
-        'postcode',
-        'phone',
-        'website',
-        'password',
-        'bio',
-        'facebook',
-        'twitter',
-        'linkedin',
-        'github',
-        'skills',
-        'locale',
-        'timezone',
-        'experience',
-        'role',
-        'facebook_id',
-        'facebook_token',
-        'google_id',
-        'google_token',
-        'github_id',
-        'github_token',
+        'name','email','address','dob','state','country','occupation','postcode','phone','website','password',
+        'bio','facebook','twitter','linkedin','github','skills','locale','timezone','experience','role','facebook_id',
+        'facebook_token','google_id','google_token','github_id','github_token',
     ];
 
     /**
@@ -76,9 +56,14 @@ class User extends Authenticatable
 
     public function jobs(): BelongsToMany
     {
+
         return $this->belongsToMany(JobListing::class, 'job_user', 'user_id', 'job_id')
             ->withTimestamps();
     }
 
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === Role::ADMIN->value;
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Enums\SkillProficiency;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,44 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@geezap.com',
             'role' => Role::ADMIN->value,
+            'password' => Hash::make('password'),
         ]);
-        User::factory(300)->create()->each(function ($user) {
-            $skillsList = [
-                'Frontend' => ['React', 'Vue.js', 'Angular', 'TypeScript', 'Tailwind CSS'],
-                'Backend' => ['Laravel', 'Node.js', 'Django', 'Spring Boot', 'Express.js'],
-                'Database' => ['MySQL', 'PostgreSQL', 'MongoDB', 'Redis', 'Elasticsearch'],
-                'DevOps' => ['Docker', 'Kubernetes', 'AWS', 'Jenkins', 'Terraform'],
-                'Mobile' => ['React Native', 'Flutter', 'Swift', 'Kotlin', 'iOS Development']
-            ];
-
-            $numberOfSkills = rand(3, 6);
-            $skills = [];
-            $skill_levels = [];
-
-
-            foreach (array_rand($skillsList, min(count($skillsList), $numberOfSkills)) as $category) {
-                $skill = $skillsList[$category][array_rand($skillsList[$category])];
-                $skills[] = $skill;
-                $skill_levels[] = fake()->randomElement([
-                    SkillProficiency::PROFICIENT->value,
-                    SkillProficiency::INTERMEDIATE->value,
-                    SkillProficiency::BEGINNER->value
-                ]);
-            }
-
-            $user->skills = json_encode([
-                'skill' => $skills,
-                'skill_level' => $skill_levels
-            ]);
-
-            $user->save();
-        });
     }
 
 }
