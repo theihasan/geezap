e3c4fe17dc8d5f7f6c9864f0b78bd9f31ff8270a
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-14 01:12:46 +0600

    add job resource to manage job listing

---
+++ app/Filament/Resources/JobListingResource.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\JobType;
+use App\Filament\Resources\JobListingResource\Pages;
+use App\Filament\Resources\JobListingResource\RelationManagers;
+use App\Models\JobListing;
+use Filament\Actions\ActionGroup;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class JobListingResource extends Resource
+{
+    protected static ?string $model = JobListing::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\Wizard::make([
+                Forms\Components\Wizard\Step::make('Basic Information')
+                    ->schema([
+                        Forms\Components\TextInput::make('job_title')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\Select::make('employment_type')
+                            ->options(JobType::class)
+                            ->required(),
+                        Forms\Components\TextInput::make('job_category')
+                            ->maxLength(255),
+                        Forms\Components\RichEditor::make('description')
+                            ->required()
+                            ->columnSpanFull(),
+                    ])->columns(2),
+
+                Forms\Components\Wizard\Step::make('Employer Details')
+                    ->schema([
+                        Forms\Components\TextInput::make('employer_name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('employer_logo')
+                            ->url()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('employer_website')
+                            ->url()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('employer_company_type')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('publisher')
+                            ->required()
+                            ->maxLength(255),
+                    ])->columns(2),
+
+                Forms\Components\Wizard\Step::make('Location & Links')
+                    ->schema([
+                        Forms\Components\Toggle::make('is_remote')
+                            ->default(false),
+                        Forms\Components\TextInput::make('city')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('state')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('country')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('apply_link')
+                            ->url()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('google_link')
+                            ->url()
+                            ->maxLength(255),
+                    ])->columns(2),
+
+                Forms\Components\Wizard\Step::make('Compensation')
+                    ->schema([
+                        Forms\Components\TextInput::make('min_salary')
+                            ->numeric(),
+                        Forms\Components\TextInput::make('max_salary')
+                            ->numeric(),
+                        Forms\Components\TextInput::make('salary_currency')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('salary_period'),
+                        Forms\Components\TagsInput::make('benefits')
+                            ->separator(','),
+                    ])->columns(2),
+
+                Forms\Components\Wizard\Step::make('Requirements & Timeline')
+                    ->schema([
+                        Forms\Components\TagsInput::make('qualifications')
+                            ->separator(','),
+                        Forms\Components\TagsInput::make('responsibilities')
+                            ->separator(','),
+                        Forms\Components\TextInput::make('required_experience')
+                            ->numeric()
+                            ->suffix('years'),
+                        Forms\Components\TagsInput::make('skills')
+                            ->separator(','),
+                        Forms\Components\DateTimePicker::make('posted_at')
+                            ->required(),
+                        Forms\Components\DateTimePicker::make('expaire_at')
+                            ->required(),
+                    ])->columns(2),
+            ])->skippable()
+        ])->columns(1);
+    }
+
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('job_title')
+                    ->limit(50),
+                Tables\Columns\TextColumn::make('publisher')
+                    ->badge()
+                    ->color('success')
+                    ->label('Source'),
+                Tables\Columns\TextColumn::make('employment_type')
+                    ->label('Type')
+                    ->color('info'),
+                Tables\Columns\TextColumn::make('posted_at')
+                    ->label('Posted Date')
+                    ->sortable()
+                    ->dateTime('jS M Y'),
+                Tables\Columns\TextColumn::make('expaire_at')
+                    ->label('Expired Date')
+                    ->sortable()
+                    ->dateTime('jS M Y')
+            ])
+            ->filters([
+
+            ])
+            ->actions([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListJobListings::route('/'),
+            'create' => Pages\CreateJobListing::route('/create'),
+            'edit' => Pages\EditJobListing::route('/{record}/edit'),
+            'view' => Pages\ViewJob::route('/{record}'),
+        ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+}
---
+++ app/Filament/Resources/JobListingResource/Pages/ViewJob.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources\JobListingResource\Pages;
+
+use App\Filament\Resources\JobListingResource;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Enums\FontWeight;
+
+class ViewJob extends ViewRecord
+{
+    protected static string $resource = JobListingResource::class;
+
+    public  function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+               Section::make('Job Overview')
+                    ->schema([
+                        TextEntry::make('job_title')
+                            ->size(TextEntry\TextEntrySize::Large)
+                            ->weight(FontWeight::Bold),
+                        TextEntry::make('employment_type')
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('job_category')
+                            ->badge(),
+                        TextEntry::make('posted_at')
+                            ->visible(fn (): bool => $this->record->posted_at !== null)
+                            ->dateTime('jS M Y'),
+                        TextEntry::make('expaire_at')
+                            ->visible(fn (): bool => $this->record->expaire_at !== null)
+                            ->dateTime('jS M Y'),
+                    ])->columns(3),
+
+                Section::make('Company Information')
+                    ->schema([
+                        ImageEntry::make('employer_logo')
+                            ->visible(fn (): bool => $this->record->employer_logo !== null)
+                            ->circular(),
+                        TextEntry::make('employer_name')
+                            ->visible(fn(): bool => $this->record->employer_name !== null),
+                        TextEntry::make('employer_company_type')
+                            ->visible(fn(): bool => $this->record->employer_company_type !== null),
+                        TextEntry::make('employer_website')
+                            ->visible(fn() : bool => $this->record->employer_website !== null),
+                        TextEntry::make('publisher')
+                            ->visible(fn() : bool => $this->record->publisher !== null),
+                    ])->columns(3),
+
+                Section::make('Job Description')
+                    ->schema([
+                        TextEntry::make('description')
+                            ->visible(fn(): bool => $this->record->description !== null)
+                            ->html()
+                            ->columnSpanFull(),
+                    ]),
+
+                Section::make('Location')
+                    ->schema([
+                        TextEntry::make('is_remote')
+                            ->label('Job Type')
+                            ->formatStateUsing(function ($state){
+                                return $state ? 'Remote' : 'On-site';
+                            })
+                            ->badge()
+                            ->color(fn ($state) => $state ? 'success' : 'gray'),
+                        TextEntry::make('city')
+                            ->visible(fn(): bool => $this->record->city !== null),
+                        TextEntry::make('state')
+                            ->visible(fn(): bool => $this->record->state !== null),
+                        TextEntry::make('country')
+                            ->visible(fn(): bool => $this->record->country !== null),
+                    ])->columns(4),
+
+                Section::make('Salary & Benefits')
+                    ->schema([
+                        TextEntry::make('min_salary')
+                            ->numeric()
+                            ->prefix(fn ($record) => $record->salary_currency),
+                        TextEntry::make('max_salary')
+                            ->visible(fn(): bool => $this->record->max_salary !== null)
+                            ->numeric()
+                            ->prefix(fn ($record) => $record->salary_currency),
+                        TextEntry::make('salary_period')
+                            ->visible(fn(): bool => $this->record->salary_period !== null),
+                        TextEntry::make('benefits')
+                            ->listWithLineBreaks(),
+                    ])->columns(2),
+
+                Section::make('Requirements & Skills')
+                    ->schema([
+                        TextEntry::make('required_experience')
+                            ->visible(fn(): bool => $this->record->required_experience !== null)
+                            ->suffix(' years'),
+                        TextEntry::make('qualifications')
+                            ->visible(fn(): bool => $this->record->qualifications !== null)
+                            ->listWithLineBreaks(),
+                        TextEntry::make('responsibilities')
+                            ->visible(fn(): bool => $this->record->responsibilities !== null)
+                            ->listWithLineBreaks(),
+                        TextEntry::make('skills')
+                            ->visible(fn(): bool => $this->record->skills !== null)
+                            ->listWithLineBreaks(),
+                    ])->columns(2),
+
+                Section::make('Application Links')
+                    ->schema([
+                        TextEntry::make('apply_link')
+                            ->visible(fn(): bool => $this->record->apply_link !== null),
+                        TextEntry::make('google_link')
+                            ->visible(fn(): bool => $this->record->google_link !== null),
+                    ])->columns(2),
+            ]);
+    }
+
+}
