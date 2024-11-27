5d91eac6af988612ff029d8c6be4aa42c2f021bf
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-27 23:20:21 +0600

    enable github login for production

--- resources/views/components/social-login.blade.php
+++ resources/views/components/social-login.blade.php
@@ -17,9 +17,8 @@
 
     <div class="relative">
         <a href="{{route('social.redirect', ['provider' => \App\Enums\SocialProvider::GITHUB->value])}}"
-           class="pointer-events-none flex items-center gap-3 bg-gray-800/50 text-white px-6 py-3 rounded-xl transition-all font-medium justify-center">
+           class="flex items-center gap-3 bg-gray-800/50 text-white px-6 py-3 rounded-xl transition-all font-medium justify-center">
             <i class="lab la-github text-xl"></i> Continue with GitHub
         </a>
-        <span class="absolute -top-2 -right-2 bg-pink-500 text-white text-xs px-2 py-1 rounded-full shadow-sm">Coming Soon</span>
     </div>
 </div>
