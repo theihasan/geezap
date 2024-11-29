123255a0b2186ac98b808285df302d633755867e
Md Abul Hassan <imabulhasan99@gmail.com>
2024-11-29 11:52:21 +0600

    add sentry for monitoring

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Sentry\Laravel\Integration;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->withSchedule(function (Schedule $schedule) {
         $schedule->job(new GetJobData())->everyMinute();
 
