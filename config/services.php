d013bc5c8cd728617908ae418901d472be50ba80
Md Abul Hassan <142471724+theihasan@users.noreply.github.com>
2024-12-02 17:18:06 +0600

    add fallback url in service list

--- config/services.php
+++ config/services.php
@@ -50,7 +50,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'redirect' => 'https://geezap.com/auth/google/callback',
     ],
 
 ];
