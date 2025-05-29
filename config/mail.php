f9b74afe67c573e6f0807a7104bb8b784ea6d0b8
Abul Hassan <imabulhasan99@gmail.com>
2025-05-29 13:28:49 +0600

    refactor(mail): reorder failover mailers priority
    Simplify failover mailers configuration by removing unused 'brevo' and 'log' transports and prioritizing 'zepto' over 'resend'

--- config/mail.php
+++ config/mail.php
@@ -104,10 +104,8 @@ return [
         'failover' => [
             'transport' => 'failover',
             'mailers' => [
-                'brevo',
-                'resend',
                 'zepto',
-                'log',
+                'resend',
             ],
         ],
 
