78bf2c8fde5199d18ac22f4e56f7832e15416fcc
Abul Hassan <imabulhasan99@gmail.com>
2025-08-29 14:59:38 +0600

    create a new package for facebook job post formating

---
+++ config/prism.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'providers' => [
+        'deepseek' => [
+            'url' => 'https://api.deepseek.com/v1',
+            'api_key' => env('DEEPSEEK_API_KEY'),
+        ],
+    ],
+];
