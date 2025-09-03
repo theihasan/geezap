d481ba893a3c4145d7be564e8cfc1aad5ca1950f
Abul Hassan <imabulhasan99@gmail.com>
2025-09-04 01:00:00 +0600

    feat: update job preferences and applications routes to use controllers for improved functionality

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,17 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\SocialAuthController;
-
 use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\JobPreferencesController;
+use App\Http\Controllers\CoverLetterController;
 
 Route::get('/', HomePageController::class)->name('home');
 Route::get('about', \App\Http\Controllers\Pages\AboutPageController::class)->name('about');
 Route::get('contact', \App\Http\Controllers\Pages\ContactPageController::class)->name('contact');
 Route::get('privacy-policy', \App\Http\Controllers\Pages\PrivacyPolicyPageController::class)->name('privacy-policy');
 Route::get('terms', \App\Http\Controllers\Pages\TermsPageController::class)->name('terms');
-Route::view('job-preferences', 'v2.pages.job-preferences')->name('job.preferences');
+Route::get('job-preferences', [JobPreferencesController::class, 'index'])->name('job.preferences');
 
 Route::prefix('jobs')->group(function () {
     Route::get('/', [JobController::class, 'index'])->name('job.index');
@@ -38,7 +40,8 @@ Route::middleware('auth')->group(function () {
     Route::post('experience', [ProfileController::class, 'updateExperience'])->name('experience.update');
     Route::post('skill', [ProfileController::class, 'updateSkill'])->name('skill.update');
 
-    Route::view('/applications', 'v2.profile.my-application')->name('applications');
+    Route::get('/applications', [ApplicationController::class, 'index'])->name('applications');
+    Route::get('/cover-letter', [CoverLetterController::class, 'update'])->name('cover-letter.update');
     Route::get('/profile/preferences', [ProfileController::class, 'preferences'])->name('profile.preferences');
     Route::post('/profile/preferences', [ProfileController::class, 'updatePreferences'])->name('profile.preferences.update');
 });
